02bb72379dc2cec4f4ebd62a94a12180d36b7fc5
Adriano Cabral de Souza <adriano.c.souza@outlook.com>
2024-08-17 14:08:55 -0300

    rollback before chromadb and vectorizer

--- app/core/utils/config.py
+++ app/core/utils/config.py
@@ -5,4 +5,3 @@ class Config:
     DW_DATABASE_URL = os.getenv("DW_DATABASE_URL")
     OLTP_DATABASE_URL = os.getenv("OLTP_DATABASE_URL")
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    VECTORIZED_DATA_PATH = os.getenv("VECTORIZED_DATA_PATH")
