db529a016ff6234851dd68104d7ea659ddd8d18e
Adriano Cabral de Souza <adriano.c.souza@outlook.com>
2024-08-17 22:13:57 -0300

    move method

--- app/core/llm/prompts.py
+++ app/core/llm/prompts.py
@@ -2,10 +2,6 @@ from langchain_core.prompts import ChatPromptTemplate
 
 
 class Prompts:
-    JSON_SCHEMA_DESCRIPTION = (
-        "{'visualizations': [{'description': 'string', 'sql_query': 'string', "
-        "'visualization': 'string', 'plotly_express_function': 'string'}]}"
-    )
 
     @staticmethod
     def data_source_overview_prompt(formatted_schema: str) -> ChatPromptTemplate:
@@ -13,19 +9,8 @@ class Prompts:
             template=(
                 f"You are connected to a database with the following schema:\n{formatted_schema}\n"
                 "Your task is to provide a clear and concise overview of this database, suitable for an end user. "
-                "The description should highlight the primary purpose and content of the database, including key tables and their relationships. "
+                "The description should highlight the primary purpose and content of the database, including key prompt ideas that a user can make it it wants to get an analysis from it, do not use markdown formatting. "
                 "Ensure that the overview is easy to understand, even for users without technical expertise."
-            )
-        )
-
-    @staticmethod
-    def clarification_prompt(query: str) -> ChatPromptTemplate:
-        return ChatPromptTemplate.from_template(
-            template=(
-                f"The user has provided the following query: '{query}'. "
-                "Please clarify the intent of this query by specifying the type of insights they might be looking for, "
-                "such as trend analysis, comparison, distribution, correlation, or a specific data point. "
-                "Also, consider any contextual details that could help tailor the analysis to the user's needs."
             )
         )
 
@@ -47,6 +32,17 @@ class Prompts:
         )
 
     @staticmethod
+    def clarification_prompt(query: str) -> ChatPromptTemplate:
+        return ChatPromptTemplate.from_template(
+            template=(
+                f"The user has provided the following query: '{query}'. "
+                "Please clarify the intent of this query by specifying the type of insights they might be looking for, "
+                "such as trend analysis, comparison, distribution, correlation, or a specific data point. "
+                "Also, consider any contextual details that could help tailor the analysis to the user's needs."
+            )
+        )
+
+    @staticmethod
     def dashboard_creation_prompt(
         formatted_schema: str, db_type: str
     ) -> ChatPromptTemplate:
@@ -57,9 +53,13 @@ class Prompts:
                 f"Here is the schema again to ensure accuracy:\n{formatted_schema}\n"
                 "Ensure that the code references tables and columns accurately according to the provided schema and is fully prepared for execution in a Streamlit app. "
                 f"Ensure that the SQL queries are fully compatible with {db_type}, avoiding syntax errors (e.g., using 'TOP' instead of 'LIMIT' for SQL Server). "
-                "Use `os.getenv('DW_DATABASE_URL')` to connect to the database. "
+                "Always use `DATABASE_URL = os.getenv('DW_DATABASE_URL')\nengine = create_engine(DATABASE_URL)` to connect to the database. "
                 "Always use SQLAlchemy to connect to the database. "
                 "Do not use or create any tables, columns, or data structures that are not explicitly mentioned in the schema provided. "
                 "The code should be concise, focused, and contain no extraneous text, comments, or markdown—only the executable Python code."
+                "Do not plot all charts one below the other, be creative with the page layout and feel free to add explaining texts to helpe the user."
+                "Also make the charts more interactive by adding dropdowns, sliders, etc."
+                "Lastly make them beautiful by adding colors, themes, etc."
+                "Do not generate tabs on the sidebar, make all dashboards one below the other with clearly separation."
             )
         )
